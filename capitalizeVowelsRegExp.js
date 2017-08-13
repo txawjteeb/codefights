3ce4b14990f3a55e91541d53dac4b5b6d90ea4df
David Lee <itxawjteeb@gmail.com>
2017-08-12 22:47:10 -0700

    solved capitalizeVowelsRegExp.js

---
+++ capitalizeVowelsRegExp.js
@@ -0,0 +1,3 @@
+function capitalizeVowelsRegExp(input) {
+    return input.replace(/[a]/gi, 'A').replace(/[e]/gi, 'E').replace(/[i]/gi, 'I').replace(/[o]/gi, 'O').replace(/[u]/gi, 'U').replace(/[y]/gi, 'Y');
+}
